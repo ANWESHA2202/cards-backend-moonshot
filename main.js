fd39353a6e5c20c78e350cf733b7bec2e57842af
Anwesha SAnyal <anwesha.sanyal22@gmail.com>
2023-07-16 13:34:26 +0530

    token generating

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
 
+//rest api routes
 
+app.post('/generate-token', async(req, res) => {
+    try {
+        const res = await fetch('https://devcore02.cimet.io/v1/generate-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+
+            },
+            body: JSON.stringify({
+                'Api-key': process.env.GENERATE_TOKEN_API_KEY,
+            })
+        })
+        const data = await res.json()
+        res.status(200).json(data)
+
+    } catch (err) {
+        res.status(500).json([{ message: err.message }])
+    }
+})
+
+//get request for testing
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
