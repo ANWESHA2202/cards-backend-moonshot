10ca18720516f8cdd4b8722d34ea8cfa30e24d85
Anwesha SAnyal <anwesha.sanyal22@gmail.com>
2023-07-16 14:00:15 +0530

    changed to imports

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 import fetch from 'node-fetch';
+
 require('dotenv').config();
 
 const app = express();
