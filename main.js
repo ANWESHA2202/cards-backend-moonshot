63bef70bebd0ca894763293ac8c6884de5dfef96
Anwesha SAnyal <anwesha.sanyal22@gmail.com>
2023-07-16 17:26:56 +0530

    get-products issue resolved

--- main.js
+++ main.js
@@ -33,20 +33,22 @@ app.post('/generate-token', async(req, res) => {
 app.post('/get-products', async(req, res) => {
     try {
         const response = await axios.post(
-            'https://devcore02.cimet.io/v1/plan-list', {}, {
+            'https://devcore02.cimet.io/v1/plan-list', {
+                "session_id": "eyJpdiI6IkVNUkZ1N0hlSHhHSnJ3Vjl4aUlxc0E9PSIsInZhbHVlIjoieFlxa1wvVDYxQWl5U2pxMDFcL0R6ZVVvdEN6Mkk0R29TRDN3ZnN0U3VGcER0cEFMa2NVb0xNcDJudjlRTHRUbGJkIiwibWFjIjoiMTE0MmU0MGE5YmJhMzY4Nzc4MDExNmZkNTI1MjZhMGE3OTQyMDZmOTc1MTVmZDM1Mzc3ZmJmNjhmMzllOGYxYSJ9"
+            }, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Api-key': process.env.GENERATE_TOKEN_API_KEY,
                     'Auth-token': req.body.token
                 },
-                body: JSON.stringify({
-                    "session_id": "eyJpdiI6IkVNUkZ1N0hlSHhHSnJ3Vjl4aUlxc0E9PSIsInZhbHVlIjoieFlxa1wvVDYxQWl5U2pxMDFcL0R6ZVVvdEN6Mkk0R29TRDN3ZnN0U3VGcER0cEFMa2NVb0xNcDJudjlRTHRUbGJkIiwibWFjIjoiMTE0MmU0MGE5YmJhMzY4Nzc4MDExNmZkNTI1MjZhMGE3OTQyMDZmOTc1MTVmZDM1Mzc3ZmJmNjhmMzllOGYxYSJ9"
-                })
+
             }
         );
         const resp_data = response.data;
+        // console.log(resp_data);
         res.status(200).json(resp_data);
     } catch (err) {
+        // console.log(err.message)
         res.status(500).json({ message: err.message });
     }
 })
